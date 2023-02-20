a4c66356a8ec4e71a830a7b8c9cff963eedea192
nurez305 <nurudeen.saliu@decagon.dev>
2023-02-20 13:12:09 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function page (){
+export default function Homepage (){
     return (
         <div>
-            <h1>Page</h1>
+            <h1 className="text-4xl">Welcome to codewitNurez Blog</h1>
             <p>Some content</p>
         </div>
     )
