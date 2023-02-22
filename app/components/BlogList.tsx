7c5a2d50f78c49a675c418e2fae13a9aedd7f9d3
nurez305 <nurudeen.saliu@decagon.dev>
2023-02-22 13:11:09 +0100

    ui update

--- app/components/BlogList.tsx
+++ app/components/BlogList.tsx
@@ -1,6 +1,7 @@
 import { Post } from "../../typings";
 import Image from "next/image";
 import urlFor from "../../lib/urlFor";
+import category from "../../schemas/category";
 
 type Props = {
   posts: Post[];
@@ -34,7 +35,20 @@ function BlogList({ posts }: Props) {
                     })}
                   </p>
                 </div>
+
+                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                    {post.categories.map((category)=>(
+                        <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                           <p>{category.title}</p> 
+                        </div>
+
+                    ))}
+                </div>
               </div>
+            </div>
+            <div className="mt-5 flex-1">
+                <p className="underline text-lg font-bold">{post.title}</p>
+                <p className="text-gray-500">{post.description}</p>
             </div>
           </div>
         ))}
