447a7ffbb89310d282c65472a7f7e0364b519188
nurez305 <nurudeen.saliu@decagon.dev>
2023-02-22 12:54:40 +0100

    fetching data from sanity

--- app/components/BlogList.tsx
+++ app/components/BlogList.tsx
@@ -1,34 +1,46 @@
-import { Post } from "../../typings"
-import Image from "next/image"
-import urlFor from "../../lib/urlFor"
+import { Post } from "../../typings";
+import Image from "next/image";
+import urlFor from "../../lib/urlFor";
 
 type Props = {
-    posts : Post[]
-}
+  posts: Post[];
+};
 
-
-function BlogList({posts}: Props) {
+function BlogList({ posts }: Props) {
   return (
     <div>
-        <hr className="border-[#f7Ab0a] mb-10"/>
+      <hr className="border-[#f7Ab0a] mb-10" />
 
-        <div>
-            {posts.map(post =>(
-                <div key={post._id}>
-                    <div>
-                        <Image 
-                        className="object-cover object-left lg:object-center"
-                        src={urlFor(post.mainImage).url()}
-                        alt={post.author.name}
-                        fill 
-                        />
-                    </div>
+      <div>
+        {posts.map((post) => (
+          <div key={post._id} className="flex flex-col group cursor-pointer">
+            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+              <Image
+                className="object-cover object-left lg:object-center"
+                src={urlFor(post.mainImage).url()}
+                alt={post.author.name}
+                fill
+              />
+
+              <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg 
+              text-white p-5 flex justify-between">
+                <div>
+                  <p className="font-bold">{post.title}</p>
+                  <p>
+                    {new Date(post._createdAt).toLocaleDateString("en-US", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </p>
                 </div>
-            ))
-            }
-        </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default BlogList
+export default BlogList;
